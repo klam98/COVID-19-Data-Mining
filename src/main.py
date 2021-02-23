6aa79a88ec0d6e3579656dd8d32e21c81e14fa98
Kenrick <kenricklam98@gmail.com>
2021-02-22 23:20:26 -0800

    Updated README, finished 1.3 on report, refreshed all plots.

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ train_processed = impute(train_processed)
 train_processed = remove_data_anomalies(train_processed)
 
 # cleaned training data has been written to "results/cases_train_processed.csv"
-train_processed.to_csv("results/cases_train_processed.csv", index=False)
+# train_processed.to_csv("results/cases_train_processed.csv", index=False)
 print("Training Data Proccessing Finished\n")
 
 print("Test Data Proccessing Start")
@@ -38,24 +38,24 @@ test_processed = clean_submitted(test)
 # test data has been fully imputed
 test_processed = impute(test_processed)
 # cleaned test data has been written to "results/cases_test_processed.csv"
-test_processed.to_csv("results/cases_test_processed.csv", index=False)
+# test_processed.to_csv("results/cases_test_processed.csv", index=False)
 print("Test Data Proccessing Finished\n")
 
 print("Remove Anomalies Start")
 location_removed = remove_location_anomalies(location)
 print("Remove Anomalies Finished\n")
-print("Locations Transform Start")
+print("Locations US Transform Start")
 location_transformed = transform_us(location_removed)
 location_transformed.to_csv("results/location_transformed.csv", index=False)
-print("Locations Transform Finished\n")
+print("Locations US Transform Finished\n")
 # extra transformation on location only for EDA purposes
-print("Locations Transform Start")
+print("Locations Countries Transform Start")
 countries_transformed = transform_countries(location_removed)
-print("Locations Transform Finished\n")
+print("Locations Countries Transform Finished\n")
 
 print("CSV Join Start")
 train_joined = join(train_processed, join_helper(location_removed), countries_transformed)
-train_joined.to_csv("results/cases_train_joined.csv", index=False)
+train_joined.to_csv("results/cases_train_processed.csv", index=False)
 test_joined = join(test_processed, join_helper(location_removed), countries_transformed)
-test_joined.to_csv("results/cases_test_joined.csv", index=False)
+test_joined.to_csv("results/cases_test_processed.csv", index=False)
 print("CSV Join Finished")
