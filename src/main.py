30acfd824607c58106a8d152e8bca23492959faf
Steven <steventktran@gmail.com>
2021-03-19 22:34:30 -0700

    Separated data into X and Y

--- src/main.py
+++ src/main.py
@@ -4,5 +4,8 @@ import sklearn as sk
 import sklearn.model_selection
 
 
-data = pd.read_csv("data/cases_train_processed.csv")
-train, test = sk.model_selection.train_test_split(data.head(5), train_size = 0.8, random_state = 0, shuffle = False, stratify = None)
+data = pd.read_csv("data/cases_train_processed.csv").to_numpy()
+Y = data[:, 9]
+X = np.delete(data, 9, 1)
+
+X_train, X_test, Y_train, Y_test = sk.model_selection.train_test_split(X, Y, train_size = 0.8, random_state = 0, shuffle = False, stratify = None)
