e33e65e11f589406da5c0420ee370adb218ec669
Kenrick <kenricklam98@gmail.com>
2021-04-25 14:17:23 -0700

    prepared cv function and report to start tuning

--- src/main.py
+++ src/main.py
@@ -68,49 +68,57 @@ def cross_validation(model, x, y):
 		'Overall Accuracy': make_scorer(accuracy_score),
 		'Overall Recall': make_scorer(recall_score, average='weighted')
 	}
-	gs = GridSearchCV(model, param_grid={'max_depth': range(2, 10, 2)},
-				  scoring=scoring, refit='F1-Score on deceased', return_train_score=True)
+	gs = GridSearchCV(model, param_grid={'max_depth': range(2, 10, 2), 'learning_rate': np.arange(0.1, 0.5, 0.1)},
+				  scoring=scoring, refit='F1-Score on deceased', return_train_score=True, n_jobs=-1)
 	gs.fit(x, y)
 	results = gs.cv_results_
-	plt.figure(figsize=(13, 13))
-	plt.title("GridSearchCV evaluating using multiple scorers simultaneously",
-			  fontsize=16)
+	print(results)
+	print()
+	print(gs.best_params_)
+	print()
+	print(gs.best_estimator_)
 
-	plt.xlabel("max_depth")
-	plt.ylabel("Score")
+	# print(results)
 
-	ax = plt.gca()
-	ax.set_xlim(0, 10)
-	ax.set_ylim(0.7, 1)
+	# plt.figure(figsize=(13, 13))
+	# plt.title("GridSearchCV evaluating using multiple scorers simultaneously",
+	# 		  fontsize=16)
 
-	# Get the regular numpy array from the MaskedArray
-	X_axis = np.array(results['param_max_depth'].data, dtype=float)
+	# plt.xlabel("max_depth")
+	# plt.ylabel("Score")
 
-	for scorer, color in zip(sorted(scoring), ['g', 'k']):
-		for sample, style in (('train', '--'), ('test', '-')):
-			sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
-			sample_score_std = results['std_%s_%s' % (sample, scorer)]
-			ax.fill_between(X_axis, sample_score_mean - sample_score_std,
-							sample_score_mean + sample_score_std,
-							alpha=0.1 if sample == 'test' else 0, color=color)
-			ax.plot(X_axis, sample_score_mean, style, color=color,
-					alpha=1 if sample == 'test' else 0.7,
-					label="%s (%s)" % (scorer, sample))
+	# ax = plt.gca()
+	# ax.set_xlim(0, 10)
+	# ax.set_ylim(0.7, 1)
 
-		best_index = np.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
-		best_score = results['mean_test_%s' % scorer][best_index]
+	# # Get the regular numpy array from the MaskedArray
+	# X_axis = np.array(results['param_max_depth'].data, dtype=float)
 
-		# Plot a dotted vertical line at the best score for that scorer marked by x
-		ax.plot([X_axis[best_index], ] * 2, [0, best_score],
-				linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+	# for scorer, color in zip(sorted(scoring), ['g', 'k']):
+	# 	for sample, style in (('train', '--'), ('test', '-')):
+	# 		sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
+	# 		sample_score_std = results['std_%s_%s' % (sample, scorer)]
+	# 		ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+	# 						sample_score_mean + sample_score_std,
+	# 						alpha=0.1 if sample == 'test' else 0, color=color)
+	# 		ax.plot(X_axis, sample_score_mean, style, color=color,
+	# 				alpha=1 if sample == 'test' else 0.7,
+	# 				label="%s (%s)" % (scorer, sample))
 
-		# Annotate the best score for that scorer
-		ax.annotate("%0.2f" % best_score,
-					(X_axis[best_index], best_score + 0.005))
+	# 	best_index = np.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
+	# 	best_score = results['mean_test_%s' % scorer][best_index]
 
-	plt.legend(loc="best")
-	plt.grid(False)
-	plt.show()
+	# 	# Plot a dotted vertical line at the best score for that scorer marked by x
+	# 	ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+	# 			linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+
+	# 	# Annotate the best score for that scorer
+	# 	ax.annotate("%0.2f" % best_score,
+	# 				(X_axis[best_index], best_score + 0.005))
+
+	# plt.legend(loc="best")
+	# plt.grid(False)
+	# plt.show()
 
 # saved_xgboost = xgboost_model(x_train, y_train)
 # saved_knn = knn_model(x_train, y_train)
